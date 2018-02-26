2173819207543f5342de7777ded10860aef99881
Sergey <grishy.ru@yandex.ru>
2018-02-26 20:51:30 +0300

    Comments

--- main.go
+++ main.go
@@ -1,44 +1,51 @@
 package main
 
 import (
-	"database/sql"
-	"html/template"
-	"log"
-	"net/http"
+	"database/sql"  // Интерфейс для работы со SQL-like БД
+	"html/template" // Шаблоны для выдачи html страниц
+	"log"           // Вывод информации в консоль
+	"net/http"      // Для запуска HTTP сервера
 
-	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/mattn/go-sqlite3" // Драйвер для работы со SQLite3
 )
 
-var (
-	DB *sql.DB
-)
+// DB указатель на соединение с базой данных
+var DB *sql.DB
 
+// Реализация обработчика запроса
 func handler(w http.ResponseWriter, r *http.Request) {
+	// Выполнение запроса к базе данных
 	rows, err := DB.Query(`SELECT name_cat FROM Category`)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer rows.Close()
 
+	// Чтение шаблона из файла
 	tmpl, _ := template.ParseFiles("tmpl/index.html")
 
+	// Создание массива для снятия слепка с базы данных,
 	names := []string{}
-
+	// Итерируемся по всем строкам, который вернул запрос SQLite
 	for rows.Next() {
 		var temp string
 		rows.Scan(&temp)
+		// Записывает возвращенные данные в слепок
 		names = append(names, temp)
 	}
 
+	// Вписываем данные в шаблон HTML страницы, дл отдачи пользователю
 	tmpl.Execute(w, names)
 }
 
 func main() {
 	var err error
+	// Открытие соединения с БД SQLite3.db
 	DB, err = sql.Open("sqlite3", "./sqlite.db")
+	// Проверка установки соединения
 	if err != nil {
 		log.Fatal(err)
 	}
+	// Закрытие соединения с БД по выходу из функции main
 	defer DB.Close()
 
 	// CREATE TABLE Category (
@@ -67,10 +74,11 @@ func main() {
 	// 	manufacture TEXT,
 	// 	weight      INTEGER
 	// );
-	fs := http.FileServer(http.Dir("static"))
+
+	// Установка обработчика запроса по данному запросу
 	http.HandleFunc("/", handler)
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	log.Println("Listening...")
+	// Запуск локального сервека на 8080 порту
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
